08978b803e98f066f141664aa52bb56f0322bb46
rostamkhani <aref.rostamkhani@gmail.com>
2024-04-03 15:18:26 +0330

    save

---
+++ build/lib/jadval/__init__.py
@@ -0,0 +1,9 @@
+from AGGrid import (print2)
+
+def main():
+    """Entry point for the application script"""
+    print("Call your main application code here")
+
+__all__ = [
+    "print2",
+]
--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup, find_packages
+import os
+
+rootpath = os.path.abspath(os.path.dirname(__file__))
+
+def read(*parts):
+    return open(os.path.join(rootpath, *parts)).read()
 
 setup(
     name='pyJadval',
@@ -6,11 +12,12 @@ setup(
     description='Displaying DataFrame data in tables with versatile and powerful features for quick and easy data exploration in notebooks, such as sorting, column rearrangement, searching, grouping, and other functionalities. A good alternative to the print function for DataFrame .',
     author='Aref Rostamkhani',
     author_email='aref.rostamkhani@gmail.com',
-    packages=find_packages(),
     keywords="data visualization",
     url="https://github.com/rostamkhani/pyJadval",
     long_description="{}".format(read("README.rst")),
     long_description_content_type="text/x-rst",
     platforms="any",
+    packages=find_packages(),
+    package_dir={'proj': 'src'}
 
 )
